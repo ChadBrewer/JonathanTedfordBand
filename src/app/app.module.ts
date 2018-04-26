7c2edc9b57c85079ab40358e2ff30a9b4d6de73a
Gavin Hill <gchill97@gmail.com>
2018-04-25 23:02:37 -0500

    Added videos and store component, and routing to each. Titled the site appropriately.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 
 import { AppComponent } from './app.component';
+import { VideosComponent } from './videos/videos.component';
+import { StoreComponent } from './store/store.component';
 
+const appRoutes: Routes = [
+    { path: 'videos', component: VideosComponent },
+    { path: 'store', component: StoreComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideosComponent,
+    StoreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
