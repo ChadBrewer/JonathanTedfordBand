e0f36219ac1dd9e530be5e1d5fd4edbfd7ff5912
Gavin Hill <gchill97@gmail.com>
2018-04-26 22:44:58 -0500

    Installed Angular Material and enabled polyfills. Made a few modifications to the Store page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-
-
 import { AppComponent } from './app.component';
 import { VideosComponent } from './videos/videos.component';
 import { StoreComponent } from './store/store.component';
 import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from "@angular/material";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
     { path: 'videos', component: VideosComponent },
@@ -26,7 +26,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
         appRoutes,
         { enableTracing: false }
-    )
+    ),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
