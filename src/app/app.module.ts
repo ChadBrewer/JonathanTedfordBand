e4e8ef761abc1361b1a7198bca5b7c6ff038e751
Gavin Hill <gchill97@gmail.com>
2018-04-26 22:15:51 -0500

    Split out the homepage content into its own component and route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { VideosComponent } from './videos/videos.component';
 import { StoreComponent } from './store/store.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
     { path: 'videos', component: VideosComponent },
-    { path: 'store', component: StoreComponent }
+    { path: 'store', component: StoreComponent },
+    { path: '', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     VideosComponent,
-    StoreComponent
+    StoreComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
