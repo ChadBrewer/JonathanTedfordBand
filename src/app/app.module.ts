30877ea5914646a85a6199d7fadbcec24d7eb3a8
U-CHADPC\Chad <Chad5972@live.missouristate.edu>
2018-05-11 22:40:22 -0500

    Removed duplicate import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { VideosComponent } from './videos/videos.component';
